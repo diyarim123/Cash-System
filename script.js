e050d85873dd9993074b986dc1495239a155cbba
DiyariMohammed <30798781+diyarim123@users.noreply.github.com>
2023-07-02 22:54:28 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+const amount = document.querySelector("#amount");
+const guests = document.querySelector("#guests");
+const quality = document.querySelector("#quality");
+const tipAmount = document.querySelector("#tip-amount");
+
+
+function calculate() {
+    const tip = ((amount.value * quality.value) / guests.value).toFixed(2);
+    amount.value = "";
+    guests.value = "";
+    quality.value = "";
+    if(tip === 'NaN') {
+        tipAmount.innerHTML = "Tip $0 each";
+        showTipAmount();
+    } else {
+        tipAmount.innerHTML = "Tip " + tip + "$ each";
+        showTipAmount();
+    }
+}
+function showTipAmount() {
+    var x = tipAmount;
+    x.className = 'show';
+    setTimeout(function() {
+        x.className = x.className.replace('show', '');
+    }, 3000)
+}
